dc6789ef4a5bf7b0e26c9025f693c315c149d240
Joseph Dela Cruz Jr <josephdelacruzjr4@gmail.com>
2026-02-14 22:45:44 +0800

    make tflash message to hooks

--- resources/js/components/app-notification.tsx
+++ resources/js/components/app-notification.tsx
@@ -1,30 +1,9 @@
-import { useEffect } from "react"
-import { usePage } from "@inertiajs/react"
-import { toast } from "sonner"
+import useFlash from "@/hooks/use-flash"
 import { Toaster } from "@/components/ui/sonner"
 
 export default function AppNotification() {
-    const { flash } = usePage().props
 
-    useEffect(() => {
-        if (!flash?.message) return
-
-        switch (flash.status) {
-            case "success":
-                toast.success(flash.message)
-                break
-            case "warning":
-                toast.warning(flash.message)
-                break
-            case "info":
-                toast.info(flash.message)
-                break
-            case "error":
-            default:
-                toast.error(flash.message)
-                break
-        }
-    }, [flash])
+    useFlash()
 
     return (
         <Toaster position="bottom-left"/>
---
+++ resources/js/hooks/use-flash.ts
@@ -0,0 +1,28 @@
+import { useEffect } from "react"
+import { usePage } from "@inertiajs/react"
+import { toast } from "sonner"
+
+export default function useFlash() {
+
+    const { flash } = usePage().props
+
+    useEffect(() => {
+        if (!flash?.message) return;
+
+        switch (flash.status) {
+            case "success":
+                toast.success(flash.message);
+                break;
+            case "warning":
+                toast.warning(flash.message);
+                break;
+            case "info":
+                toast.info(flash.message);
+                break;
+            case "error":
+            default:
+                toast.error(flash.message);
+                break;
+        }
+    }, [flash]);
+}
